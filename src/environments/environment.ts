931725db6beff3c6ceb8b0de9dca42078c04a6b4
nstitou <nstitou@kaisensdata.fr>
2023-10-02 13:57:35 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'smart-home-5107f',
+    appId: '1:1061516208044:web:22a5c7daa1fe6fb195fc09',
+    databaseURL: 'https://smart-home-5107f-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'smart-home-5107f.appspot.com',
+    apiKey: 'AIzaSyAZvO0NX6LdIDgAx4K277TM7Ctq4i24qHY',
+    authDomain: 'smart-home-5107f.firebaseapp.com',
+    messagingSenderId: '1061516208044',
+  },
+};
